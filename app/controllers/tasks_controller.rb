b74cd336df2385243787b7a73286f310d466cb93
Esteban Montantes <esmo.ledev@gmail.com>
2018-11-06 16:36:39 -0500

    edit update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,49 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
+  end
+
+  def create
+    # POST /tasks
+    # Create a task in the DB
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    # GET /tasks/new
+    # Render a form
+    @task = Task.new
+  end
+
+  def edit
+    # GET /tasks/:id/edit
+    # Render a form for edditing
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # PATCH /tasks/:id
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # DELETE /tasks/:id
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
